8591f6ab6c41c8ba912c342d94ee8adfeb3ed434
Verox001 <altergeist987@gmail.com>
2021-05-30 13:40:06 +0200

    .

---
+++ Fluid/Core/Network/Packets/Packet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fluid.Core.Network.Packets
+{
+    class Packet
+    {
+        private ReadOnlyMemory<byte> _buffer;
+
+        protected MemoryStreamReader lol;
+        public Packet(ReadOnlyMemory<byte> buffer)
+        {
+            _buffer = buffer;
+        }
+    }
+}
--- Fluid/Core/Network/Packets/PacketSender.cs
+++ Fluid/Core/Network/Packets/PacketSender.cs
@@ -1,10 +1,21 @@
+using Fluid.Core.Network;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
 namespace Fluid.Core
 {
     /// <summary>
     /// Allows sending packets to other clients
+    /// From NiclasOlofsson
     /// </summary>
-    public static class PacketSender
+    public interface PacketSender
     {
-        
+        public void SendData(byte[] data, IPEndPoint targetEndPoint);
+        public Task SendDataAsync(byte[] data, IPEndPoint targetEndPoint);
+        public Task SendDataAsync(byte[] data, int length, IPEndPoint targetEndPoint);
+        public Task SendPacketAsync(Session session, Packet message);
+        public Task SendPacketAsync(Session session, List<Packet> message);
+        void Close(Session session);
     }
 }
