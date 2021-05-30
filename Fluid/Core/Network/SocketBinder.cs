189cb7bd937a3c9a6eaa8f1a9ccedef23c006c6e
Verox001 <altergeist987@gmail.com>
2021-05-30 13:14:33 +0200

    Added simple Endpoint

---
+++ Fluid/Core/Network/SocketBinder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fluid.Core.Network
+{
+    public class SocketBinder
+    {
+        public IPEndPoint EndPoint { get; private set; }
+
+        public SocketBinder(IPEndPoint endpoint)
+        {
+            EndPoint = endpoint;
+        }
+
+        /// <summary>
+        /// Creates the Endpoint for the Server
+        /// </summary>
+        public bool CreateEndpoint()
+        {
+            IPAddress IPAddress = Dns.Resolve("0.0.0.0").AddressList[0];
+            EndPoint = new IPEndPoint(IPAddress, 19132);
+            if(EndPoint == null)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
