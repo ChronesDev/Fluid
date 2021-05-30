777f726c1d4f04dbb4eadaa99cd63521289d9105
CXCubeHD <cxcubehd@gmail.com>
2021-05-30 15:06:06 +0200

    fixed RakNet.cs

--- Fluid/Core/Network/RakNet/RakNet.cs
+++ Fluid/Core/Network/RakNet/RakNet.cs
@@ -8,39 +8,28 @@ namespace Fluid.Core.Network.RakNet
 {
     public abstract class RakNet
     {
-        public static string VERSION = "1.0";
-        public static byte PROTOCOL = 6;
+        public const string Version = "1.0";
+        public const byte Protocol = 6;
 
-        public static byte[] PACKETS = new byte[]
+        public const byte PacketEncapsulate  = 0x01;
+        public const byte PacketOpenSession = 0x02;
+        public const byte PacketCloseSession = 0x03;
+        public const byte PacketInvalidSession = 0x04;
+        public const byte PacketSendQueue = 0x05;
+        public const byte PacketAckNotification = 0x06;
+        public const byte PacketSetOption = 0x07;
+        public const byte PacketRaw = 0x08;
+        public const byte PacketBlockAddress = 0x09;
+        public const byte PacketUnblockAddress = 0x10;
+        public const byte PacketShutdown = 0x7e;
+        public const byte PacketEmergencyShutdown = 0x7f;
+        
+        public static readonly byte[] PACKETS = new byte[]
         {
             (byte) 0x00, (byte) 0xff, (byte) 0xff, (byte) 0x00,
             (byte) 0xfe, (byte) 0xfe, (byte) 0xfe, (byte) 0xfe,
             (byte) 0xfd, (byte) 0xfd, (byte) 0xfd, (byte) 0xfd,
             (byte) 0x12, (byte) 0x34, (byte) 0x56, (byte) 0x78
         };
-
-        public static byte PACKET_ENCAPSULATE = 0x01;
-
-        public static byte PACKET_OPEN_SESSION = 0x02;
-
-        public static  byte PACKET_CLOSE_SESSION = 0x03;
-
-        public static byte PACKET_INVALID_SESSION = 0x04;
-
-        public static byte PACKET_SEND_QUEUE = 0x05;
-
-        public static byte PACKET_ACK_NOTIFICATION = 0x06;
-
-        public static byte PACKET_SET_OPTION = 0x07;
-
-        public static byte PACKET_RAW = 0x08;
-
-        public static byte PACKET_BLOCK_ADDRESS = 0x09;
-
-        public static byte PACKET_UNBLOCK_ADDRESS = 0x10;
-
-        public static byte PACKET_SHUTDOWN = 0x7e;
-
-        public static byte PACKET_EMERGENCY_SHUTDOWN = 0x7f;
     }
 }
