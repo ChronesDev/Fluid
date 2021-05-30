ca64bd3965a7cf5572cca653cbc6c9c61453c1d2
Verox001 <altergeist987@gmail.com>
2021-05-30 14:07:42 +0200

    I think I checked something

--- Fluid/Core/Network/RakNet/RakNet.cs
+++ Fluid/Core/Network/RakNet/RakNet.cs
@@ -10,5 +10,15 @@ namespace Fluid.Core.Network.RakNet
     {
         public static string VERSION = "1.0";
         public static byte PROTOCOL = 6;
+
+        public static byte[] BYTES = new byte[]
+        {
+            (byte) 0x00, (byte) 0xff, (byte) 0xff, (byte) 0x00,
+            (byte) 0xfe, (byte) 0xfe, (byte) 0xfe, (byte) 0xfe,
+            (byte) 0xfd, (byte) 0xfd, (byte) 0xfd, (byte) 0xfd,
+            (byte) 0x12, (byte) 0x34, (byte) 0x56, (byte) 0x78
+        };
+
+        public static byte PACKET_ENCAPSULATED = 0x01;
     }
 }
