a9eccd7d9d2bbc4ec722564a12bc3301d8f698b9
Verox001 <altergeist987@gmail.com>
2021-05-30 13:45:03 +0200

    How I can program Packet.cs

--- Fluid/Core/Network/Packets/Packet.cs
+++ Fluid/Core/Network/Packets/Packet.cs
@@ -8,12 +8,6 @@ namespace Fluid.Core.Network.Packets
 {
     class Packet
     {
-        private ReadOnlyMemory<byte> _buffer;
-
-        protected MemoryStreamReader lol
-        public Packet(ReadOnlyMemory<byte> buffer)
-        {
-            _buffer = buffer;
-        }
+        
     }
 }
