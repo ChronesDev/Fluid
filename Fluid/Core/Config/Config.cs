a152602787c017c34b37c0444d6ea67f06ebe9ad
SilasTheCoder <39573757+SilasTheCoder@users.noreply.github.com>
2021-05-31 18:14:31 -0500

    Added basic JSON writing.

--- Fluid/Core/Config/Config.cs
+++ Fluid/Core/Config/Config.cs
@@ -1,7 +1,35 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
 namespace Fluid.Core
 {
     public class Config
     {
-        
+        public static bool WriteConfig()
+        {
+            if (!File.Exists("properties.json"))
+            {
+                var properties = new Dictionary<string, string>() 
+                {
+                    {"ip", "0.0.0.0"},
+                    {"port", "19132"},
+                    {"whitelist", "false"},
+                    {"online_mode", "true"},
+                    {"view_distance", "8"},
+                    {"max_players", "20"},
+                    {"motd", "Fluid Server"}
+                };
+                
+                var options = new JsonSerializerOptions { WriteIndented = true };
+                string pretty = JsonSerializer.Serialize(properties, options);
+                File.WriteAllText("properties.json", pretty);
+                return true;
+            }
+            else
+            {
+                return true;
+            }
+        }
     }
 }
