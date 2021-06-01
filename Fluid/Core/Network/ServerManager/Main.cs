ebb4fb1dee2ddc1e35d3f97ec6b34d61a0f1235a
Verox001 <altergeist987@gmail.com>
2021-06-01 16:49:43 +0200

    update

--- Fluid/Core/Network/ServerManager/Main.cs
+++ Fluid/Core/Network/ServerManager/Main.cs
@@ -1,4 +1,5 @@
 ﻿using Fluid.Core.Logger;
+using Fluid.Core.Network.Protocol;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,313 +12,96 @@ namespace Fluid.Core
 {
     public class Main
     {
-        public delegate void PacketHandler(RemoteClient client, Main server, IPacket packet);
 
-        #region Constructors
+        private DataPack[] PacketPool { get; set; }
 
-        public Main()
+        public void registerPacket(byte id, DataPack pack)
         {
-            PacketHandlers = new Dictionary<Type, PacketHandler>();
-            ServerLogger.Info("Started");
-            Core.PacketHandlers.RegisterHandlers(this);
-            NetworkLock = new object();
-            Clients = new List<RemoteClient>();
-            LastTimeUpdate = DateTime.MinValue;
-            NextChunkUpdate = DateTime.MinValue;
+            this.PacketPool[id & 0xff] = pack;
         }
 
-        #endregion
-        
-        #region Properties
-
-        public List<RemoteClient> Clients { get; set; }
-        public TcpListener Listener { get; set; }
-        public DateTime StartTime { get; private set; }
-
-        protected internal RSACryptoServiceProvider CryptoServiceProvider { get; set; }
-        protected internal RSAParameters ServerKey { get; set; }
-        protected internal object NetworkLock { get; set; }
-
-        protected Thread NetworkThread { get; set; }
-        protected Dictionary<Type, PacketHandler> PacketHandlers { get; set; }
-
-        private DateTime NextPlayerUpdate { get; set; }
-        private DateTime NextChunkUpdate { get; set; }
-        private DateTime LastTimeUpdate { get; set; }
-        private DateTime NextScheduledSave { get; set; }
-
-        private const int _millisecondsBetweenPhysicsUpdates = 250;
-
-        #endregion
-
-        #region Public methods
-
-        public void RegisterPacketHandler(Type packetType, PacketHandler handler)
+        public void registerPackets()
         {
-            if (!typeof(IPacket).IsAssignableFrom(packetType))
-                throw new ArgumentException("Packet type must derive from Craft.Net.Networking.IPacket");
-            PacketHandlers[packetType] = handler;
-        }
-
-        public void Start(IPEndPoint endPoint)
-        {
-            /**if (30 != -1)
-                NextScheduledSave = DateTime.Now.AddSeconds(30);
-
-            CryptoServiceProvider = new RSACryptoServiceProvider(1024);
-            ServerKey = CryptoServiceProvider.ExportParameters(true);
-
-            StartTime = DateTime.Now;
-
-            Listener = new TcpListener(endPoint);
-            Listener.Start();
-            Listener.BeginAcceptTcpClient(AcceptClientAsync, null);
-
-            NetworkThread = new Thread(NetworkWorker);
-            NetworkThread.Start();**/
-
-
-        }
-
-        public void Stop()
-        {
-            lock (NetworkLock)
-            {
-                if (Listener != null)
-                {
-                    Listener.Stop();
-                    Listener = null;
-                }
-                if (NetworkThread != null)
-                {
-                    NetworkThread.Abort();
-                    NetworkThread = null;
-                }
-            }
-        }
-
-        public RemoteClient GetClient(string name)
-        {
-            return Clients.SingleOrDefault(c => c.Username == name);
-        }
-
-        public void DisconnectPlayer(RemoteClient client, string reason = null)
-        {
-            if (!Clients.Contains(client))
-                throw new InvalidOperationException("The server is not aware of this client.");
-            lock (NetworkLock)
-            {
-                if (reason != null)
-                {
-                    try
-                    {
-                        if (client.NetworkClient != null && client.NetworkClient.Connected)
-                        {
-                            if (client.NetworkManager.NetworkMode == NetworkMode.Login)
-                                client.NetworkManager.WritePacket(new LoginDisconnectPacket("\"" + reason + "\""), PacketDirection.Clientbound);
-                            else
-                                client.NetworkManager.WritePacket(new DisconnectPacket("\"" + reason + "\""), PacketDirection.Clientbound);
-                        }
-                    }
-                    catch { }
-                }
-                try
-                {
-                    if (client.NetworkClient != null && client.NetworkClient.Connected)
-                    {
-                        client.NetworkClient.Close();
-                    }
-                }
-                catch { }
-                Clients.Remove(client);
-                if (client.IsLoggedIn)
-                {
-                    var args = new PlayerLogInEventArgs(client);
-                    OnPlayerLoggedOut(args);
-                }
-            }
-        }
-
-        #endregion
-
-        #region Protected methods
-
-        protected void AcceptClientAsync(IAsyncResult result)
-        {
-            lock (NetworkLock)
-            {
-                if (Listener == null)
-                    return; // Server shutting down
-                var client = new RemoteClient(Listener.EndAcceptTcpClient(result));
-                client.NetworkStream = client.NetworkClient.GetStream();
-                client.NetworkManager = new NetworkManager(client.NetworkStream);
-                Clients.Add(client);
-                Listener.BeginAcceptTcpClient(AcceptClientAsync, null);
-            }
-        }
-
-        protected internal void UpdatePlayerList()
-        {
-            if (Clients.Count != 0)
-            {
-                for (int i = 0; i < Clients.Count; i++)
-                {
-                    foreach (RemoteClient client in Clients)
-                    {
-                        if (client.IsLoggedIn)
-                            Clients[i].SendPacket(new PlayerListItemPacket(client.Username, true, client.Ping));
-                    }
-                }
-            }
-        }
-
-        #endregion
-
-        #region Internal methods
-
-        internal void LogInPlayer(RemoteClient client)
-        {
-            client.SendPacket(new LoginSuccessPacket(client.UUID, client.Username));
-
-        }
-
-        #endregion
-
-        #region Private methods
-
-        private void NetworkWorker()
-        {
-            while (true)
-            {
-                UpdateScheduledEvents();
-                lock (NetworkLock)
-                {
-                    for (int i = 0; i < Clients.Count; i++)
-                    {
-                        var client = Clients[i];
-                        bool disconnect = false;
-                        while (client.PacketQueue.Count != 0)
-                        {
-                            IPacket nextPacket;
-                            if (client.PacketQueue.TryDequeue(out nextPacket))
-                            {
-                                try
-                                {
-                                    client.NetworkManager.WritePacket(nextPacket, PacketDirection.Clientbound);
-                                }
-                                catch (System.IO.IOException)
-                                {
-                                    disconnect = true;
-                                    continue;
-                                }
-                                if (nextPacket is DisconnectPacket // TODO: This could be cleaner
-                                    || (nextPacket is StatusPingPacket && client.NetworkManager.NetworkMode == NetworkMode.Status))
-                                {
-                                    disconnect = true;
-                                }
-                            }
-                        }
-                        if (disconnect)
-                        {
-                            DisconnectPlayer(client);
-                            i--;
-                            continue;
-                        }
-                        // Read packets
-                        var timeout = DateTime.Now.AddMilliseconds(10);
-                        while (client.NetworkClient.Available != 0 && DateTime.Now < timeout)
-                        {
-                            try
-                            {
-                                var packet = client.NetworkManager.ReadPacket(PacketDirection.Serverbound);
-                                if (packet is DisconnectPacket)
-                                {
-                                    DisconnectPlayer(client);
-                                    i--;
-                                    break;
-                                }
-                                HandlePacket(client, packet);
-                            }
-                            catch (SocketException)
-                            {
-                                DisconnectPlayer(client);
-                                i--;
-                                break;
-                            }
-//                            catch (Exception e)
-//                            {
-//                                DisconnectPlayer(client, e.Message);
-//                                i--;
-//                                break;
-//                            }
-                        }
-                        if (client.IsLoggedIn)
-                            DoClientUpdates(client);
-                    }
-                }
-                if (LastTimeUpdate != DateTime.MinValue)
-                {
-                    if ((DateTime.Now - LastTimeUpdate).TotalMilliseconds >= 50)
-                    {
-                        LastTimeUpdate = DateTime.Now;
-                    }
-                }
-                if (NextChunkUpdate < DateTime.Now)
-                    NextChunkUpdate = DateTime.Now.AddSeconds(1);
-                Thread.Sleep(10);
-            }
-        }
-
-        private void HandlePacket(RemoteClient client, IPacket packet)
-        {
-            if (!PacketHandlers.ContainsKey(packet.GetType()))
-                return;
-                //throw new InvalidOperationException("No packet handler registered for 0x" + packet.Id.ToString("X2"));
-            PacketHandlers[packet.GetType()](client, this, packet);
-        }
-
-        private void UpdateScheduledEvents()
-        {
-            if (DateTime.Now > NextPlayerUpdate)
-            {
-                UpdatePlayerList();
-                NextPlayerUpdate = DateTime.Now.AddMinutes(1);
-            }
-        }
-
-        private void DoClientUpdates(RemoteClient client)
-        {
-            // Update keep alive, chunks, etc
-            if (client.LastKeepAliveSent.AddSeconds(20) < DateTime.Now)
-            {
-                client.SendPacket(new KeepAlivePacket(FluidMath.Random.Next()));
-                client.LastKeepAliveSent = DateTime.Now;
-                // TODO: Confirm keep alive
-            }
-        }
-
-        #endregion
-
-        #region Events
-
-        public event EventHandler<ConnectionEstablishedEventArgs> ConnectionEstablished;
-        protected internal virtual void OnConnectionEstablished(ConnectionEstablishedEventArgs e)
-        {
-            if (ConnectionEstablished != null) ConnectionEstablished(this, e);
-        }
-
-        public event EventHandler<PlayerLogInEventArgs> PlayerLoggedIn;
-        protected internal virtual void OnPlayerLoggedIn(PlayerLogInEventArgs e)
-        {
-            if (PlayerLoggedIn != null) PlayerLoggedIn(this, e);
-        }
-
-        public event EventHandler<PlayerLogInEventArgs> PlayerLoggedOut;
-        protected internal virtual void OnPlayerLoggedOut(PlayerLogInEventArgs e)
-        {
-            if (PlayerLoggedOut != null) PlayerLoggedOut(this, e);
-        }
-
-        #endregion
+            PacketPool = new DataPack[256];
+            registerPacket(ProtocolInfo.ADD_ENTITY_PACKET, AddEntityPacket.class);
+            registerPacket(ProtocolInfo.ADD_ITEM_ENTITY_PACKET, AddItemEntityPacket.class);
+            registerPacket(ProtocolInfo.ADD_PAINTING_PACKET, AddPaintingPacket.class);
+            registerPacket(ProtocolInfo.ADD_PLAYER_PACKET, AddPlayerPacket.class);
+            registerPacket(ProtocolInfo.ADVENTURE_SETTINGS_PACKET, AdventureSettingsPacket.class);
+            registerPacket(ProtocolInfo.ANIMATE_PACKET, AnimatePacket.class);
+            registerPacket(ProtocolInfo.AVAILABLE_COMMANDS_PACKET, AvailableCommandsPacket.class);
+            registerPacket(ProtocolInfo.BATCH_PACKET, BatchPacket.class);
+            registerPacket(ProtocolInfo.BLOCK_ENTITY_DATA_PACKET, BlockEntityDataPacket.class);
+        this.registerPacket(ProtocolInfo.BLOCK_EVENT_PACKET, BlockEventPacket.class);
+        this.registerPacket(ProtocolInfo.BLOCK_PICK_REQUEST_PACKET, BlockPickRequestPacket.class);
+        this.registerPacket(ProtocolInfo.BOSS_EVENT_PACKET, BossEventPacket.class);
+        this.registerPacket(ProtocolInfo.CHANGE_DIMENSION_PACKET, ChangeDimensionPacket.class);
+        this.registerPacket(ProtocolInfo.CHUNK_RADIUS_UPDATED_PACKET, ChunkRadiusUpdatedPacket.class);
+        this.registerPacket(ProtocolInfo.CLIENTBOUND_MAP_ITEM_DATA_PACKET, ClientboundMapItemDataPacket.class);
+        this.registerPacket(ProtocolInfo.COMMAND_REQUEST_PACKET, CommandRequestPacket.class);
+        this.registerPacket(ProtocolInfo.CONTAINER_CLOSE_PACKET, ContainerClosePacket.class);
+        this.registerPacket(ProtocolInfo.CONTAINER_OPEN_PACKET, ContainerOpenPacket.class);
+        this.registerPacket(ProtocolInfo.CONTAINER_SET_DATA_PACKET, ContainerSetDataPacket.class);
+        this.registerPacket(ProtocolInfo.CRAFTING_DATA_PACKET, CraftingDataPacket.class);
+        this.registerPacket(ProtocolInfo.CRAFTING_EVENT_PACKET, CraftingEventPacket.class);
+        this.registerPacket(ProtocolInfo.DISCONNECT_PACKET, DisconnectPacket.class);
+        this.registerPacket(ProtocolInfo.ENTITY_EVENT_PACKET, EntityEventPacket.class);
+        this.registerPacket(ProtocolInfo.ENTITY_FALL_PACKET, EntityFallPacket.class);
+        this.registerPacket(ProtocolInfo.EXPLODE_PACKET, ExplodePacket.class);
+        this.registerPacket(ProtocolInfo.FULL_CHUNK_DATA_PACKET, FullChunkDataPacket.class);
+        this.registerPacket(ProtocolInfo.GAME_RULES_CHANGED_PACKET, GameRulesChangedPacket.class);
+        this.registerPacket(ProtocolInfo.HURT_ARMOR_PACKET, HurtArmorPacket.class);
+        this.registerPacket(ProtocolInfo.INTERACT_PACKET, InteractPacket.class);
+        this.registerPacket(ProtocolInfo.INVENTORY_CONTENT_PACKET, InventoryContentPacket.class);
+        this.registerPacket(ProtocolInfo.INVENTORY_SLOT_PACKET, InventorySlotPacket.class);
+        this.registerPacket(ProtocolInfo.INVENTORY_TRANSACTION_PACKET, InventoryTransactionPacket.class);
+        this.registerPacket(ProtocolInfo.ITEM_FRAME_DROP_ITEM_PACKET, ItemFrameDropItemPacket.class);
+        this.registerPacket(ProtocolInfo.LEVEL_EVENT_PACKET, LevelEventPacket.class);
+        this.registerPacket(ProtocolInfo.LEVEL_SOUND_EVENT_PACKET, LevelSoundEventPacket.class);
+        this.registerPacket(ProtocolInfo.LOGIN_PACKET, LoginPacket.class);
+        this.registerPacket(ProtocolInfo.MAP_INFO_REQUEST_PACKET, MapInfoRequestPacket.class);
+        this.registerPacket(ProtocolInfo.MOB_ARMOR_EQUIPMENT_PACKET, MobArmorEquipmentPacket.class);
+        this.registerPacket(ProtocolInfo.MOB_EQUIPMENT_PACKET, MobEquipmentPacket.class);
+        this.registerPacket(ProtocolInfo.MODAL_FORM_REQUEST_PACKET, ModalFormRequestPacket.class);
+        this.registerPacket(ProtocolInfo.MODAL_FORM_RESPONSE_PACKET, ModalFormResponsePacket.class);
+        this.registerPacket(ProtocolInfo.MOVE_ENTITY_PACKET, MoveEntityPacket.class);
+        this.registerPacket(ProtocolInfo.MOVE_PLAYER_PACKET, MovePlayerPacket.class);
+        this.registerPacket(ProtocolInfo.PLAYER_ACTION_PACKET, PlayerActionPacket.class);
+        this.registerPacket(ProtocolInfo.PLAYER_INPUT_PACKET, PlayerInputPacket.class);
+        this.registerPacket(ProtocolInfo.PLAYER_LIST_PACKET, PlayerListPacket.class);
+        this.registerPacket(ProtocolInfo.PLAYER_HOTBAR_PACKET, PlayerListPacket.class);
+        this.registerPacket(ProtocolInfo.PLAY_SOUND_PACKET, PlaySoundPacket.class);
+        this.registerPacket(ProtocolInfo.PLAY_STATUS_PACKET, PlayStatusPacket.class);
+        this.registerPacket(ProtocolInfo.REMOVE_ENTITY_PACKET, RemoveEntityPacket.class);
+        this.registerPacket(ProtocolInfo.REQUEST_CHUNK_RADIUS_PACKET, RequestChunkRadiusPacket.class);
+        this.registerPacket(ProtocolInfo.RESOURCE_PACKS_INFO_PACKET, ResourcePacksInfoPacket.class);
+        this.registerPacket(ProtocolInfo.RESOURCE_PACK_STACK_PACKET, ResourcePackStackPacket.class);
+        this.registerPacket(ProtocolInfo.RESOURCE_PACK_CLIENT_RESPONSE_PACKET, ResourcePackClientResponsePacket.class);
+        this.registerPacket(ProtocolInfo.RESOURCE_PACK_DATA_INFO_PACKET, ResourcePackDataInfoPacket.class);
+        this.registerPacket(ProtocolInfo.RESOURCE_PACK_CHUNK_DATA_PACKET, ResourcePackChunkDataPacket.class);
+        this.registerPacket(ProtocolInfo.RESOURCE_PACK_CHUNK_REQUEST_PACKET, ResourcePackChunkRequestPacket.class);
+        this.registerPacket(ProtocolInfo.RESPAWN_PACKET, RespawnPacket.class);
+        this.registerPacket(ProtocolInfo.RIDER_JUMP_PACKET, RiderJumpPacket.class);
+        this.registerPacket(ProtocolInfo.SET_COMMANDS_ENABLED_PACKET, SetCommandsEnabledPacket.class);
+        this.registerPacket(ProtocolInfo.SET_DIFFICULTY_PACKET, SetDifficultyPacket.class);
+        this.registerPacket(ProtocolInfo.SET_ENTITY_DATA_PACKET, SetEntityDataPacket.class);
+        this.registerPacket(ProtocolInfo.SET_ENTITY_LINK_PACKET, SetEntityLinkPacket.class);
+        this.registerPacket(ProtocolInfo.SET_ENTITY_MOTION_PACKET, SetEntityMotionPacket.class);
+        this.registerPacket(ProtocolInfo.SET_HEALTH_PACKET, SetHealthPacket.class);
+        this.registerPacket(ProtocolInfo.SET_PLAYER_GAME_TYPE_PACKET, SetPlayerGameTypePacket.class);
+        this.registerPacket(ProtocolInfo.SET_SPAWN_POSITION_PACKET, SetSpawnPositionPacket.class);
+        this.registerPacket(ProtocolInfo.SET_TITLE_PACKET, SetTitlePacket.class);
+        this.registerPacket(ProtocolInfo.SET_TIME_PACKET, SetTimePacket.class);
+        this.registerPacket(ProtocolInfo.SERVER_SETTINGS_REQUEST_PACKET, ServerSettingsRequestPacket.class);
+        this.registerPacket(ProtocolInfo.SERVER_SETTINGS_RESPONSE_PACKET, ServerSettingsResponsePacket.class);
+        this.registerPacket(ProtocolInfo.SHOW_CREDITS_PACKET, ShowCreditsPacket.class);
+        this.registerPacket(ProtocolInfo.SPAWN_EXPERIENCE_ORB_PACKET, SpawnExperienceOrbPacket.class);
+        this.registerPacket(ProtocolInfo.START_GAME_PACKET, StartGamePacket.class);
+        this.registerPacket(ProtocolInfo.TAKE_ITEM_ENTITY_PACKET, TakeItemEntityPacket.class);
+        this.registerPacket(ProtocolInfo.TEXT_PACKET, TextPacket.class);
+        this.registerPacket(ProtocolInfo.UPDATE_ATTRIBUTES_PACKET, UpdateAttributesPacket.class);
+        this.registerPacket(ProtocolInfo.UPDATE_BLOCK_PACKET, UpdateBlockPacket.class);
+        this.registerPacket(ProtocolInfo.UPDATE_TRADE_PACKET, UpdateTradePacket.class);
     }
+}
 }
