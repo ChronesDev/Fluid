9e6e1d0c167912f66cfb05cb15107cf13a596e14
CXCubeHD <cxcubehd@gmail.com>
2021-06-02 11:14:57 +0200

    Extended the PlayerInventory class

--- Fluid/Core/Inventory/Inventories/PlayerInventory.cs
+++ Fluid/Core/Inventory/Inventories/PlayerInventory.cs
@@ -3,5 +3,17 @@ namespace Fluid.Core
     public record PlayerInventory : Inventory
     {
         public static PlayerInventory Empty => new();
+        
+        public override bool Is<T>() => typeof(T) == typeof(PlayerInventory);
+
+        public override int SlotCount => Slots.Length;
+        public override ItemSlot[] AllSlots => Slots;
+        public override ref ItemSlot GetSlot(int index) => ref Slots[index];
+        public override void SetSlot(ItemSlot slot, int index) => Slots[index] = slot;
+
+        /// <summary>
+        /// Stores all ItemSlots
+        /// </summary>
+        public ItemSlot[] Slots { get; private set; } = new ItemSlot[36];
     }
 }
